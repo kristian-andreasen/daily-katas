5a98fabb0fdd9a670ceba00f5bac1f2a903b3535
Kristian Andreasen <kristian.andreasen@fi.experis.com>
2023-01-12 10:06:17 +0100

    added comments

--- day1.js
+++ day1.js
@@ -1,8 +1,10 @@
+//the function takes a string and separates into three different strings
 function reorderString(str) {
   let uppercaseLetters = "";
   let lowercaseLetters = "";
   let numbers = "";
-
+  
+  //iterate through each charachter in the string
   for (let char of str) {
     if (isNaN(char)) {
       if (char == char.toUpperCase()) {
@@ -18,9 +20,11 @@ function reorderString(str) {
       numbers = numbers + char;
     }
   }
+  //concatenate the strings, and return it
   return uppercaseLetters + lowercaseLetters + numbers;
 }
 
+//Test input strings
 word1 = "hA2p4Py";
 word2 = "m11oveMENT";
 word3 = "s9hOrt4CAKE";
